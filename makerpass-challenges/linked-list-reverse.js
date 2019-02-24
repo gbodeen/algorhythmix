a1b60837b1b755db5023da6327e9ec7a8e677dd2
Gabriel Bodeen <gabebodeen@gmail.com>
2019-02-24 13:52:57 -0600

    New: reverseLinkedList in makerpass-challenges/linked-list-reverse.

---
+++ makerpass-challenges/linked-list-reverse.js
@@ -0,0 +1,26 @@
+/* 
+Write a function that reverses a linked list.
+Constraint 1: Do this in linear time.
+Constraint 2: Do this in constant space.
+Constraint 3: Do not mutate the original nodes by adding any new properties.
+*/
+
+var reverseLinkedList = function (linkedList) {
+  let current = linkedList, prev = null;
+  let next;
+  while (current) {
+    next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  return prev;
+};
+// Meets complexity & other constraints.
+
+// tests
+var testval = { "value": "A", "next": null };
+console.log(reverseLinkedList(testval) === testval); // unchanged in this case
+testval = { "value": "A", "next": { "value": "B", "next": { "value": "C", "next": { "value": "D", "next": { "value": "E", "next": null } } } } };
+console.log(reverseLinkedList(testval));
+// {"value":"E","next":{"value":"D","next":{"value":"C","next":{"value":"B","next":{"value":"A","next":null}}}}};
