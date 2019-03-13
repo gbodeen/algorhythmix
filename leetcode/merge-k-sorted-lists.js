9304a8998d70aa03c425d2a6dae34b54ee1301f1
Gabriel Bodeen <gabebodeen@gmail.com>
2019-03-12 23:11:41 -0500

    New: mergeKLists in leetcode/merge-k-sorted-lists.

---
+++ leetcode/merge-k-sorted-lists.js
@@ -0,0 +1,49 @@
+/*
+Merge k sorted linked lists and return it as one 
+sorted list. Analyze and describe its complexity.
+*/
+
+var mergeKLists = function (lists) {
+  let idx = mindex(lists.map(head => head.val));
+  let newlist = lists[idx];
+  if (lists[idx].next) {
+    lists[idx] = lists[idx].next;
+  } else {
+    lists.splice(idx, 1);
+  }
+  let node = newlist;
+  while (lists.length) {
+    idx = mindex(lists.map(head => head.val));
+    node.next = lists[idx];
+    node = node.next;
+    if (lists[idx].next) {
+      lists[idx] = lists[idx].next;
+    } else {
+      lists.splice(idx, 1);
+    }
+  }
+  return newlist;
+};
+
+function mindex(list) {
+  let min = Infinity, idx = -1;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] < min) {
+      min = list[i];
+      idx = i;
+    }
+  }
+  return idx;
+}
+
+
+// tests
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+let foo = new ListNode(1);
+foo.next = new ListNode(3);
+foo.next.next = new ListNode(5);
+let bar = new ListNode(0);
+console.log(mergeKLists([foo, bar]));
