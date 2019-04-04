4c59c0cc93337e83ea9aeed2c1ebf9d20cfce76d
Gabriel Bodeen <gabebodeen@gmail.com>
2019-04-04 17:33:51 -0500

    New: Java PE001 in project-euler.

--- java/project-euler/PE001.java
+++ java/project-euler/PE001.java
@@ -6,8 +6,14 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 */
 
 public class PE001 {
-
   public static void main(String[] args) {
-    System.out.println("Testing");
+    int top = 1000;
+    int sum = 0;
+    for (int i = 1; i < top; i++) {
+      if (i % 3 == 0 || i % 5 == 0) {
+        sum += i;
+      }
+    }
+    System.out.println(sum); // 233168
   }
 }
